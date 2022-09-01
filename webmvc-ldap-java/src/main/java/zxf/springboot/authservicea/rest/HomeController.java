d6c1dd4fa789cddc1218ef7518f2131d620d13ad
davis <zanxiaofeng@163.com>
2022-09-01 21:14:21 +0800

    update

--- webmvc-ldap-java/src/main/java/zxf/springboot/authservicea/rest/HomeController.java
+++ webmvc-ldap-java/src/main/java/zxf/springboot/authservicea/rest/HomeController.java
@@ -1,7 +1,6 @@
 package zxf.springboot.authservicea.rest;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.ldap.userdetails.LdapUserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
